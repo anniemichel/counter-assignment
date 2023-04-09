aa0fd07b9be213a1824e7b1246bb58a94e41b3e1
anniemichel <anne.michel@ufl.edu>
2023-04-09 15:13:55 -0400

    adding test cases for counter functionality

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,28 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter count/>);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const initialCount = screen.getByTestId('count');
+  expect(initialCount).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByRole('button', {name: '+' }));
+  const counterDisplay = screen.getByTestId('count');
+  expect(counterDisplay).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByRole('button', {name: '-' }));
+  const counterDisplay = screen.getByTestId('count');
+  expect(counterDisplay).toHaveTextContent('-1');
 });
